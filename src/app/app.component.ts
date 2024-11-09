1997f6e13e82124faa096eb4b985a0f83fd64897
Naomill <saraleedrschool@gmail.com>
2024-11-10 02:47:56 +0700

    create search box component and housing component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
   template: `
-    <h1>Hello world</h1>
+    <main>
+      <header class="brand-name">
+        <h1 class="primary-color"><i class="fas fa-home"></i> Home</h1>
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
   `,
   styleUrls: ['./app.component.css'],
+  imports: [ HomeComponent ],
 })
 export class AppComponent {
   title = 'Homes';
